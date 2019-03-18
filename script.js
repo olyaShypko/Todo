f7a66954ce65d2df0d1e3cb0b1adc4fc887043a0
olyaShypko <47418937+olyaShypko@users.noreply.github.com>
2019-03-18 22:00:26 +0200

    new version

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 window.onload = function() {
 
-  var all_list = [];
-  if(localStorage.getItem('text') !=undefined){
-    all_list = JSON.parse(localStorage.getItem('text'));
-    out();
-  }
-  document.getElementById("add").onclick = function() {
-    var note = document.getElementById("note").value;
-    var list = {};
-    list.text = note;
-    list.check = false;
-    var i = all_list.length;
-    all_list[i] = list;
-    console.log("note",all_list);
-    out();
-    localStorage.setItem("text",JSON.stringify(all_list));
-  }
+  // var all_list = [];
+  // if(localStorage.getItem('text') !=undefined){
+  //   all_list = JSON.parse(localStorage.getItem('text'));
+  //   out();
+  // }
+  // document.getElementById("add").onclick = function() {
+  //   var note = document.getElementById("note").value;
+  //   var list = {};
+  //   list.text = note;
+  //   list.check = false;
+  //   var i = all_list.length;
+  //   all_list[i] = list;
+  //   console.log("note",all_list);
+  //   out();
+  //   localStorage.setItem("text",JSON.stringify(all_list));
+  // }
 
-  function out(){
-    var out = "";
-    var div = "";
-    for(var key in all_list){
-      if(all_list[key].check == true){
-        div += "<input type = 'checkbox' checked>"
-      }else{
-        div += "<input type = 'checkbox' >"
-      }
-      div = document.createElement('div');
-      div.innerHTML = all_list[key].text;
+  // function out(){
+  //   var out = "";
+  //   var div = "";
+  //   for(var key in all_list){
+  //     if(all_list[key].check == true){
+  //       div += "<input type = 'checkbox' checked>"
+  //     }else{
+  //       div += "<input type = 'checkbox' >"
+  //     }
+  //     div = document.createElement('div');
+  //     div.innerHTML = all_list[key].text;
 
-      // out += all_list[key].text + '<br>'
+  //     // out += all_list[key].text + '<br>'
+  //   }
+  //   document.body.appendChild(div);
+  //   // document.getElementById("show_notes").innerHTML = out;
+  // }
+ var add = document.getElementById("add");
+    add.onclick = function(){
+      var gen_windiw = document.createElement('div');
+      gen_windiw.classList.add("general");
+      document.body.appendChild(gen_windiw);
+
+      var p = document.createElement("p");
+      p.innerHTML = document.getElementById("note").value;
+      gen_windiw.appendChild(p);
+
+      var checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      gen_windiw.appendChild(checkbox);
+
+      var span = document.createElement("span");
+      span.classList.add("close");
+      gen_windiw.appendChild(span);
+    } 
+var span =  document.getElementsByTag(span); 
+    span.onclick = function() {
+         span.parentNode.remove();
+
     }
-    document.body.appendChild(div);
-    // document.getElementById("show_notes").innerHTML = out;
-  }
 }
